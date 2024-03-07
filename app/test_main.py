b2475cf4d0e3ad62c881a2a5ce5309bc34361e50
Maksim <sma4no.7@gmail.com>
2024-03-07 10:28:07 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "template, expected_result",
+    [
+        pytest.param(
+            "Flfm@fk34sjkgu@4t45h5223466fg6h6",
+            False
+        ),
+        pytest.param(
+            "skdign24@",
+            False
+        ),
+        pytest.param(
+            "Dfjfjjfksiw@jd",
+            False
+        ),
+        pytest.param(
+            "sdF23FFFFF",
+            False
+        ),
+        pytest.param(
+            "fF@1",
+            False
+        )
+    ]
+)
+def test_check_password(template: str, expected_result: bool) -> None:
+    assert check_password(template) == expected_result
